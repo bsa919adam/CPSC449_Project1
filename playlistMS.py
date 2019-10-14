36d6687b4b8edc55e8f9dd9e5df3fea9f2963a69
student <bsa919adam@gmail.com>
2019-10-13 17:37:29 -0700

    finished add new need to test everything

--- playlistMS.py
+++ playlistMS.py
@@ -82,18 +82,24 @@ def playlists():
     else:
         return delete(request.args)
 
-#TODO
+#TODO  Test 
 def retrieve(args):
     s_query, s_qargs= createQuery(args)
     s_query= "SELECT * FROM playlists "+ s_query
     if len(s_qargs) == 0:
         return {"MESASGE":'Please enter values to search for'}, status.HTTP_404_NOT_FOUND
-    
+
+    result={}
     try:
         result = query_db(s_query, s_qargs)
     except Exception as e:
             return {'error': str(e)}, status.HTTP_404_NOT_FOUND
     
+    result = appendTracks(result)
+
+    return result, status.HTTP_200_OK 
+
+def appendTracks(result):
     for x in result:
         id = x.get(id)
         if not id:
@@ -103,10 +109,7 @@ def retrieve(args):
         qargs=[id]
         tracks= query_db(query, qargs)
         x.update({"tracks":tracks}) 
-
-    return result, status.HTTP_200_OKAY 
-
-
+    return result
 
 #TODO Test delete
 def delete(args):
@@ -121,15 +124,86 @@ def delete(args):
     d_query=s_query.relplace("SELECT id", "DELETE")
     query_db(d_query, s_qargs)
 
-    return {'MESSAGE':'Playlist deleted'}, status.HTTP_200_OKAY
+    return {'MESSAGE':'Playlist deleted'}, status.HTTP_200_OK
 
 
-#TODO
+#TODO Test
 @app.route('/api/v1/playlists/all', methods=["GET"])
 def all_playlists():
-    pass
+    s_query= "SELECT * FROM playlists"
+    result={}
+    
+    try:
+        result = query_db(s_query)
+    except Exception as e:
+            return {'error': str(e)}, status.HTTP_404_NOT_FOUND
+    
+    result = appendTracks(result)
+    return result, status.HTTP_200_OK 
 
-#TODO
+
+#TODO Test
 @app.route('/api/v1/playlists/<user>', methods=['GET'])
-def user_playlists():
-    pass
+def user_playlists(user):
+    s_qargs=[user]
+    s_query= "SELECT * FROM playlists WHERE creator=?"
+    if len(s_qargs) == 0:
+        return {"MESASGE":'Please enter values to search for'}, status.HTTP_404_NOT_FOUND
+
+    result={}
+    try:
+        result = query_db(s_query, s_qargs)
+    except Exception as e:
+            return {'error': str(e)}, status.HTTP_404_NOT_FOUND
+    
+    result = appendTracks(result)
+
+    return result, status.HTTP_200_OK 
+
+
+#TODO Test
+#json input format
+#{
+#     title:"title",
+#     creator:"creator",
+#     description:"description",
+# #   tracks:{
+#             1:url
+#             2:url
+# #   } 
+# # }
+@app.route('/api/v1/playlists/new', methods=['GET','POST'])
+def new_playlist():
+    if request.method =='POST':
+
+        required=['title', 'creator', 'tracks']
+        if not all([fields in request.data for fields in required]):
+            raise exceptions.ParseError()
+
+        temp , q_args = createQuery(request.data)
+        query = "Insert into playlists(title, creator, Description) VALUES(?,?,?);"
+        desc = request.data.get("description")
+        if  art: 
+            pass
+        else:
+            q_args.append("NONE")         
+
+        try:
+            query_db(query, q_args)
+        except Exception as e:
+            return {'error': str(e)}, status.HTTP_409_CONFLICT
+        
+        query="SELECT id FROM playlists " +temp
+        id = query_db(query, q_args).get('id')
+        tracks = request.data.get('tracks')
+        query= "INSERT INTO playlist_tracks(playlist_id, track_url) VALUES(?,?)"
+        for x in tracks:
+            try:
+                query_db(query, [id, tracks.get(x))
+            except Exception as e:
+                return {'error': str(e)}, status.HTTP_409_CONFLICT
+        
+        return {"MESSAGE": "Playlist Created"}, status.HTTP_201_CREATED
+    else:
+       
+        return  {"INFO":"Enter Data For New Playlist"}, status.HTTP_200_OK
