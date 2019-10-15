b21897d032b2a5b687e69218afefd14b593b96ca
AdolfoAlirio <47457565+AdolfoAlirio@users.noreply.github.com>
2019-10-15 12:44:34 -0700

    updated

--- script.py
+++ script.py
@@ -3,59 +3,39 @@ import requests
 import resp as resp
 import tracks as tracks
 
-trackdata ={
-    'title':'title'
-    'artist':'artist'
-    'length':'length'
-    'link':'track_url'
-}
-
 newtrack={
-    'title': 'title'
-    'artist': 'artist'
-    'album': 'album'
-    'length': 'length'
-    'description': 'description'
+    'title': 'title',
+    'artist': 'artist',
+    'album': 'album',
+    'length': 'length',
+    'description': 'description',
 }
 
-playlistdata={
-    'playlist_url':'playlist_url'
-    'title': 'title'
-    'artist': 'artist'
-    'album': 'album'
-    'length': 'length'
-    'description': 'description'
-}
-userdata={
-    'username': 'username'
-    'displayname':'displayname'
+newplaylistdata={
+'playlist_url':'playlist_url',
 }
 
-newplaylistdata{
-'playlist_url':'playlist_url'
-}
-
-tracks = requests.get('/api/v1/tracks', trackdata,json = tracks)
+tracks = requests.get('/api/v1/tracks',newtrack,json = tracks)
 if tracks.status_code != 200:
     raise ApiError('GET /tracks {}'.format(tracks.status_code))
-    print(tracks,trackdata.json)
+    print(tracks,newtrack=newtrack.json)
 
-new = requests.post('/api/v1/tracks/new', newtrack, json = new)
+new = requests.post('/api/v1/tracks/new', json = new)
 if tracks.status_code != 200:
     raise ApiError('GET /tracks {}'.format(new.status_code))
-    print(new,newtrack.json)
+    print(new)
 
-playlist = request.get('/api/v1/playlists/all',playlistdata,json = playlist)
+playlist = request.get('/api/v1/playlists/all',json = playlist)
 if playlist.status_code != 200:
     raise ApiError('GET /all {}'.format(rest.status_code))
-    print(playlist,playlistdata)
+    print(playlist)
 
-user = requests.get('/api/v1/playlists/<user>', userdata,json = user)
+user = requests.get('/api/v1/playlists/<user>',json = user)
 if user.status_code != 200:
     raise ApiError('GET /user {}'.format(user.status_code))
-    print(user,userdata.json)
+    print(user)
 
 newplaylist = request.post('/api/v1/playlists/new',newplaylistdata, json = newplaylist)
 if newplaylist.status_code != 200:
     raise ApiError('POST /new {}'.format(newplaylist.status_code))
-    print(newplaylist,newplaylistdata.json)
+    print(newplaylist,newplaylistdata = newplaylistdata.json)
